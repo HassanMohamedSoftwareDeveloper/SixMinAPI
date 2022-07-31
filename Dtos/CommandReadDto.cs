88cbef750aadbe9979e26bff6058d69398218946
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-07-31 19:29:27 +0200

    Add Models and Dtos

---
+++ Dtos/CommandReadDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SixMinAPI.Dtos;
+
+public class CommandReadDto
+{
+    public int Id { get; set; }
+    public string HowTo { get; set; }
+    public string Platform { get; set; }
+    public string CommandLine { get; set; }
+}
---
+++ Dtos/CommandUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SixMinAPI.Dtos;
+
+public class CommandUpdateDto
+{
+    [Required] public string HowTo { get; set; }
+    [Required][MaxLength(5)] public string Platform { get; set; }
+    [Required] public string CommandLine { get; set; }
+}
