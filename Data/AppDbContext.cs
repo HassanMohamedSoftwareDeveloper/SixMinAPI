365a94f0df20a8df54d3e8720ff6e3cb56315fe1
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-07-31 19:49:57 +0200

    Add AppDbContext and connection string

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SixMinAPI.Models;
+
+namespace SixMinAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Command> Commands => Set<Command>();
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SixMinAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var sqlConBuilder = new SqlConnectionStringBuilder
+{
+    ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection"),
+    UserID = builder.Configuration["UserId"],
+    Password = builder.Configuration["Password"],
+};
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConBuilder.ConnectionString));
 
 var app = builder.Build();
 
